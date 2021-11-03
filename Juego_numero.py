a04c7bb91011c62de28024dcb16e2357dffde585
guerramorante <mmorante@myuax.com>
2021-11-03 19:14:11 +0100

    main

---
+++ Juego_numero.py
@@ -0,0 +1,17 @@
+import random
+numero = random.randint (0,100)
+print("\t.: Adivina el numero")
+numero_de_intentos = 0
+while True:
+    numero1 = int(input("Introduzca un número:"))
+    numero_de_intentos += 1
+    if numero1 == numero:
+       print("¡Esta es la respuesta!") 
+       break
+    else:
+        if numero1 > numero:
+           print("Prueba con un numero menor")
+        else: 
+           print("Prueba con un numero mayor")
+print(f"\nNúmero de intentos: {numero_de_intentos}")
+
