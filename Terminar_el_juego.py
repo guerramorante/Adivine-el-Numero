8d40a045b7d0036892ecbe25c72a75e2a4c3361e
guerramorante <mmorante@myuax.com>
2021-11-13 12:23:04 +0100

    main

---
+++ Terminar_el_juego.py
@@ -0,0 +1,40 @@
+import random
+while True:
+    print("Adivina el numero")
+    print("""Escoja nivel de dificultad
+Nivel 1 simple: entre 0 y 100
+Nivel 2 intermedio: entre 0 y 1000
+Nivel 3 avanzado: entre 0 y 1000000
+Nivel 4 experto: entre 0 y 1000000000000""")
+    level=int(input("Escriba su opción (de 1 a 4): "))
+    if level <=4:
+        if level ==1:
+            print("Has seleccionado el nivel simple, tienes que adivinar un numero entre 0 y 100 y solo tienes 15 intentos")
+        elif level ==2:
+            print("Has seleccionado el nivel intermedio, tienes que adivinar un numero entre 0 y 1000 y solo tienes 35 intentos")
+        elif level ==3:
+            print("Has seleccinado el nivel avanzado, tienes que adivinar un numero entre 0 y 1000000 y solo tienes 70")
+        elif level ==4:
+            print("Has seleccionado el nivel experto, tienes que adivinar un numero entre 0 y 1000000000000 y tienes 190 intentos")
+    else:
+        print("Este nivel no existe")
+    if level == 1:
+       import random 
+       numero=random.randint (0,100)
+       print("\t.: Escoja un numero")
+       numero_de_intentos = 0
+       while True:
+        numero1 = int(input("Introduzca un número del 1 al 100:"))
+        numero_de_intentos += 1
+        mumero_de_intentos_max = 15
+        if numero1 == numero:
+           print("¡Esta es la respuesta!") 
+           break
+        else:
+            if numero1 > numero:
+               print("Prueba con un numero menor")
+            else: 
+               print("Prueba con un numero mayor")
+       print(f"\nNúmero de intentos: {numero_de_intentos}")
+
+
